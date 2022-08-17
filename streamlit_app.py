20e2208c67e8f6afb6f85179d66917bcce4d5c8a
vijayganeshsivaraman <111388228+vijayganeshsivaraman@users.noreply.github.com>
2022-08-17 08:32:28 +0100

    Update streamlit_app.py
    added import pandas and fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 #created the main python file
 
 import streamlit
+import pandas
 
 streamlit.title('My Moms new healthy diner')
 streamlit.header('Breakfast Favorites')
@@ -10,5 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
    
